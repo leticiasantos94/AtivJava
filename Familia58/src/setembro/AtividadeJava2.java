2a2a1c6817c376439c10ee2693467557e268682c
leticiasantos94 <leestudos94@gmail.com>
2022-09-09 08:54:00 -0300

    Primeiros exercícios Java

---
+++ Familia58/src/setembro/AtividadeJava2.java
@@ -0,0 +1,27 @@
+package setembro;
+
+import java.util.Scanner;
+
+public class AtividadeJava2 {
+
+	public static void main(String[] args) {
+		
+		int numero;
+		double resultado;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("\nEntre com um número qualquer: ");
+		numero = leia.nextInt();
+		
+		if(numero % 2 == 0) {
+			resultado = Math.sqrt(numero);
+		}else {
+			resultado = Math.pow(numero, 2);
+		}
+		
+		System.out.println("\nResultado: "+resultado);
+
+	}
+
+}
